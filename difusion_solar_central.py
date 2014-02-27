e4873921757274ac006e48268fe29585654f75d4
Fabio Marquez <fa.marquez2294@compufi1.uniandes.edu.co>
2014-02-27 13:15:54 -0500

    3,b) Difusion solar central

---
+++ difusion_solar_central.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+m=sys.argv[1]
+n=int(m)
+
+x=np.zeros(n)
+y=np.zeros(n)
+z=np.zeros(n)
+Pasos=np.zeros(n)
+Distancia=np.zeros(n)
+
+for i in range(n):
+  while Distancia[i]<100:
+    theta=2.0*(np.pi*np.random.random())
+    phi=2.0*(np.pi*np.random.random())
+    x[i]=x[i]+((np.sin(theta))*(np.cos(phi)))
+    y[i]=y[i]+(np.sin(theta))*(np.sin(phi))
+    z[i]=z[i]+(np.cos(theta))
+    Pasos[i]=Pasos[i]+1
+    Distancia[i]=np.sqrt(x[i]**2 + y[i]**2 + z[i]**2)
+
+plt.hist(Pasos, bins=50)
+plt.show()
